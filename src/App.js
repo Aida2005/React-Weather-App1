a08e67b8e630450c92c36ead076c0751f1cf1ddb
Aida <aydahamedi56@gmail.com>
2022-02-28 14:18:46 -0800

    Added the basic css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-   <h1> Weather App </h1>
+   <Weather />
     <footer>
       This project was coded by Aida Hamedi and is{" "}
       <a href="https://github.com/Aida2005/React-Weather-App">
